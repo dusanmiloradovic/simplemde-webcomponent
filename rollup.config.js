2ec7a9cfaa9c6963731d581df9843a5af2eaac8c
Dusan Miloradovic <dusan.m@maximoplus.com>
2022-05-26 08:12:07 +0400

    remove unnecessary dev libs

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import typescript from "rollup-plugin-typescript2";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import css from "rollup-plugin-import-css";
-import postcssurl from "postcss-url";
-import postcss from "rollup-plugin-postcss";
 export default {
   input: "src/index.ts",
   output: {
